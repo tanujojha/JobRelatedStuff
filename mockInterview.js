590351c99f4a9410a073fcf8e02f91013076bea3
Tanuj Ojha <tanujojha2042@gmail.com>
2024-08-17 11:04:24 +0530

    init commit

---
+++ mockInterview.js
@@ -0,0 +1,10 @@
+
+
+const api = "https://gitlab.com/gvanderput/gerard-movie-filtering/-/raw/master/data/movies.json";
+
+file:///C:/Users/Dell/Downloads/React%20Interview%20Questions.pdf
+file:///C:/Users/Dell/Downloads/Node%20interview%20questions.pdf
+https://docs.google.com/document/d/1n9Ro8c9FfbCu949LQ-C4Xw8bsQDxBXCmKoC0tmQFX2w/edit
+
+// Interview Tips : https://docs.google.com/document/d/1-CxSsv-XzYExNHwYBU7FlKklw6_FEMCUPV7XqoaXRos/edit
+
